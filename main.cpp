091083c696bbb7d24514b5c9f07288b58dcff26c
Darya833 <kyrbobadarya@mail.ru>
2025-10-13 12:40:03 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+{int_1=10;
+cout << " int_1 = " << int_1 << " | тип: int, размер 4 6. " << endl;
+int_2=5
+cout << " int_2 = " << int_2 << " | тип: int, размер 4 6. << endl;
+int_3=0 
+cout << " int_3 = " << int_3 << " | тип: int, размер 4 6. << endl;
+int_4=26
+cout << " int_4 = " << int_4 << " | тип: int, размер 4 6. << endl;
+int_5=-6
+cout << " int_5 = " << int_5 << " | тип: int, размер 4 6. << endl;
+}
+{short s_1 = 1;
+cout << "s_1 =  " << s_1 << " | тип: short, размер: 2 6. << endl;
+  short s_2 = 234;
+cout << "s_2 =  " << s_2 << " | тип: short, размер: 2 6. << endl;
+  short s_3 = 1467;
+cout << "s_3 =  " << s_3 << " | тип: short, размер: 2 6. << endl;
+   short s_4 = 3;
+cout << "s_4 =  " << s_4 << " | тип: short, размер: 2 6. << endl;
+   short s_5 = -34;
+cout << "s_5=  " << s_5 << " | тип: short, размер: 2 6. << endl;
+  }  
