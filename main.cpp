6f6778ce4c1ab9f20fbe5e9ef838a73d3ac08255
Darya833 <kyrbobadarya@mail.ru>
2025-10-14 23:13:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,4 +20,27 @@ cout << "s_4 =  " << s_4 << " | тип: short, размер: 2 6. << endl;
    short s_5 = -34;
 cout << "s_5=  " << s_5 << " | тип: short, размер: 2 6. << endl;
   }  
+ {
+    string_1 = " 3A72";
+ cout << string_1 = " << string_1 << " | тип: string, размер: динамичный. " << endl;
+    string_2 = " Bye";
+ cout << string_2 = " << string_2 << " | тип: string, размер: динамичный. " << endl;
+    string_3 = " 34K";
+ cout << string_3 = " << string_3 << " | тип: string, размер: динамичный. " << endl;
+    string_4 = " 48F";
+ cout << string_4 = " << string_4 << " | тип: string, размер: динамичный. " << endl;
+    string_5 = " 8L6";
+ cout << string_5 = " << string_5 << " | тип: string, размер: динамичный. " << endl;
+      }
 {
+    char_1 = "3";
+cout << " char_1 = " << char_1 << " | тип: char, размер: 1 б. " << endl;  
+    char_2 = "T";
+cout << " char_2 = " << char_2 << " | тип: char, размер: 1 б. " << endl;  
+    char_3 = "7";
+cout << " char_3 = " << char_3 << " | тип: char, размер: 1 б. " << endl;  
+    char_4 = "A";
+cout << " char_4 = " << char_4 << " | тип: char, размер: 1 б. " << endl;
+    char_5 = "9";
+cout << " char_5 = " << char_5 << " | тип: char, размер: 1 б. " << endl;  
+ }
